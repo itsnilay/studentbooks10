5b68d7c9d3bfab1482d297012c485d9cf26debb1
itsnilay <itsnilay@a2928fc4-1f20-58ce-a954-0d9b33963282>
2010-11-12 12:42:38 +0000

    Book Listings Page now connected with DB + Added to resolve conflict + Changes Induced

--- SpindleTree/books_listing.php
+++ SpindleTree/books_listing.php
@@ -1,8 +1,6 @@
 <?php
-$page_title ='SpindleTree | Search Results';
-$page_special="BOOKS";
+$page_title ='XXX';
 include('include/header.php');
-$page_special="";
 
 require_once("include/mysql_connect.php");
 
@@ -19,11 +17,11 @@ $book_synopsis = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed 
 $book_expert_rating = 3.5;
 $new_book_price = 45.90;
 $used_book_price = 28.86;
-$book_cover_tiny = "img/51Zy0q83ipL._AA200_.jpg";
+$book_cover_tiny = "img/51Zy0q83ipL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA300_SH20_OU01_.jpg";
 $ccsavings = array(13.05, 18.86, 5.07, 3.86, 21.70, 18.60, 24.37, 12.34, 24.35, 8.00);*/
 ?>
-    <h2>Search Results</h2>
-    <div class="span-18 last">
+    <h1>Books Listing</h1>
+    <div class="span-19 last">
         <select class="span-5">
             <option class="first" value="">Choose a School...</option>
             <option value="">School #1</option>
@@ -42,9 +40,9 @@ $ccsavings = array(13.05, 18.86, 5.07, 3.86, 21.70, 18.60, 24.37, 12.34, 24.35, 
         </select>
     </div>
 
-    <div class="span-18">
-        <ul id="books_list" class="span-18 last">
-            <li class="book odd span-18 last">
+    <div class="span-19">
+        <ul id="books_list" class="span-19 last">
+            <li class="book odd span-19 last">
 
             <?php
             $result = SpindleTreeDB::getInstance()->get_all_books();
@@ -60,238 +58,28 @@ $ccsavings = array(13.05, 18.86, 5.07, 3.86, 21.70, 18.60, 24.37, 12.34, 24.35, 
                 $ccsavings = "13.05";
             ?>
 
-                <div class="span-18 last">
+                <div class="span-19 last">
                     <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
                     <span id="author"><?php echo $book_author; ?></span>
                 </div>
-                <div class="span-18 last">
+                <div class="span-19 last">
                     <div class="span-3">
                         <a href="used_book_listing.php"><img class="span-3" src="include/getBLOB.php?id=<?php echo $bookid ?>"></a>
                     </div>
                     <div class="span-3">
-<<<<<<< .mine
                         <p>Used: <span class="used_price">$<?php echo $used_book_price ?></span></p>
                         <p>New: <span class="new_price">$<?php echo $used_book_price ?></span></p>
-=======
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
->>>>>>> .r53
                     </div>
                     <div class="span-6">
-<<<<<<< .mine
                         <p>Save <span class="cc_savings"><?php echo $ccsavings?> </span> on the price at your bookstore!</p>
-=======
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[0]);?> </span> on the price at your bookstore!</p>
->>>>>>> .r53
                     </div>
-                    <div class="span-6 last">
+                    <div class="span-7 last">
                         <button type="submit" value="book-<?php echo 0 ?>">+ Add to Cart</button>
                     </div>
                 </div>
                 <? }
             ?>
             </li>
-<<<<<<< .mine
-=======
-            <!-- ITEM NUMBER 1 //-->
-            <li class="book even span-18 last">
-                <div class="span-18 last">
-                    <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
-                    <span id="author"><?php echo $book_author_last . ', ' . $book_author_first; ?></span>
-                </div>
-                <div class="span-18 last">
-                    <div class="span-3">
-                        <a href="used_book_listing.php"><img class="span-3" src="<?php echo $book_cover_tiny ?>"/></a>
-                    </div>
-                    <div class="span-3">
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
-                    </div>
-                    <div class="span-6">
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[1]);?> </span> on the price at your bookstore!</p>
-                    </div>
-                    <div class="span-6 last">
-                        <button class="add_to_cart" type="submit" value="book-<?php echo 1 ?>">+ Add to Cart</button>
-                    </div>
-                </div>
-            </li>
-            <!-- ITEM NUMBER 2 //-->
-            <li class="book odd span-18 last">
-                <div class="span-18 last">
-                    <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
-                    <span id="author"><?php echo $book_author_last . ', ' . $book_author_first; ?></span>
-                </div>
-                <div class="span-18 last">
-                    <div class="span-3">
-                        <a href="used_book_listing.php"><img class="span-3" src="<?php echo $book_cover_tiny ?>"/></a>
-                    </div>
-                    <div class="span-3">
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
-                    </div>
-                    <div class="span-6">
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[2]);?> </span> on the price at your bookstore!</p>
-                    </div>
-                    <div class="span-6 last">
-                         <button class="add_to_cart" type="submit" value="book-<?php echo 2 ?>">+ Add to Cart</button>
-                    </div>
-                </div>
-            </li>
-            <!-- ITEM NUMBER 3 //-->
-            <li class="book even span-18 last">
-                <div class="span-18 last">
-                    <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
-                    <span id="author"><?php echo $book_author_last . ', ' . $book_author_first; ?></span>
-                </div>
-                <div class="span-18 last">
-                    <div class="span-3">
-                        <a href="used_book_listing.php"><img class="span-3" src="<?php echo $book_cover_tiny ?>"/></a>
-                    </div>
-                    <div class="span-3">
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
-                    </div>
-                    <div class="span-6">
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[3]);?> </span> on the price at your bookstore!</p>
-                    </div>
-                    <div class="span-6 last">
-                         <button class="add_to_cart" type="submit" value="book-<?php echo 3 ?>">+ Add to Cart</button>
-                    </div>
-                </div>
-            </li>
-            <!-- ITEM NUMBER 4 //-->
-            <li class="book odd span-18 last">
-                <div class="span-18 last">
-                    <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
-                    <span id="author"><?php echo $book_author_last . ', ' . $book_author_first; ?></span>
-                </div>
-                <div class="span-18 last">
-                    <div class="span-3">
-                        <a href="used_book_listing.php"><img class="span-3" src="<?php echo $book_cover_tiny ?>"/></a>
-                    </div>
-                    <div class="span-3">
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
-                    </div>
-                    <div class="span-6">
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[4]);?> </span> on the price at your bookstore!</p>
-                    </div>
-                    <div class="span-6 last">
-                         <button class="add_to_cart" type="submit" value="book-<?php echo 4 ?>">+ Add to Cart</button>
-                    </div>
-                </div>
-            </li>
-            <!-- ITEM NUMBER 5 //-->
-            <li class="book even span-18 last">
-                <div class="span-18 last">
-                    <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
-                    <span id="author"><?php echo $book_author_last . ', ' . $book_author_first; ?></span>
-                </div>
-                <div class="span-18 last">
-                    <div class="span-3">
-                        <a href="used_book_listing.php"><img class="span-3" src="<?php echo $book_cover_tiny ?>"/></a>
-                    </div>
-                    <div class="span-3">
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
-                    </div>
-                    <div class="span-6">
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[5]);?> </span> on the price at your bookstore!</p>
-                    </div>
-                    <div class="span-6 last">
-                         <button class="add_to_cart" type="submit" value="book-<?php echo 5 ?>">+ Add to Cart</button>
-                    </div>
-                </div>
-            </li>
-            <!-- ITEM NUMBER 6 //-->
-            <li class="book odd span-18 last">
-                <div class="span-18 last">
-                    <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
-                    <span id="author"><?php echo $book_author_last . ', ' . $book_author_first; ?></span>
-                </div>
-                <div class="span-18 last">
-                    <div class="span-3">
-                        <a href="used_book_listing.php"><img class="span-3" src="<?php echo $book_cover_tiny ?>"/></a>
-                    </div>
-                    <div class="span-3">
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
-                    </div>
-                    <div class="span-6">
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[6]);?> </span> on the price at your bookstore!</p>
-                    </div>
-                    <div class="span-6 last">
-                         <button class="add_to_cart" type="submit" value="book-<?php echo 6 ?>">+ Add to Cart</button>
-                    </div>
-                </div>
-            </li>
-            <!-- ITEM NUMBER 7 //-->
-            <li class="book even span-18 last">
-                <div class="span-18 last">
-                    <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
-                    <span id="author"><?php echo $book_author_last . ', ' . $book_author_first; ?></span>
-                </div>
-                <div class="span-18 last">
-                    <div class="span-3">
-                        <a href="used_book_listing.php"><img class="span-3" src="<?php echo $book_cover_tiny ?>"/></a>
-                    </div>
-                    <div class="span-3">
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
-                    </div>
-                    <div class="span-6">
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[7]);?> </span> on the price at your bookstore!</p>
-                    </div>
-                    <div class="span-6 last">
-                         <button class="add_to_cart" type="submit" value="book-<?php echo 7 ?>">+ Add to Cart</button>
-                    </div>
-                </div>
-            </li>
-            <!-- ITEM NUMBER 8 //-->
-            <li class="book odd span-18 last">
-                <div class="span-18 last">
-                    <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
-                    <span id="author"><?php echo $book_author_last . ', ' . $book_author_first; ?></span>
-                </div>
-                <div class="span-18 last">
-                    <div class="span-3">
-                        <a href="used_book_listing.php"><img class="span-3" src="<?php echo $book_cover_tiny ?>"/></a>
-                    </div>
-                    <div class="span-3">
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
-                    </div>
-                    <div class="span-6">
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[8]);?> </span> on the price at your bookstore!</p>
-                    </div>
-                    <div class="span-6 last">
-                         <button class="add_to_cart" type="submit" value="book-<?php echo 8 ?>">+ Add to Cart</button>
-                    </div>
-                </div>
-            </li>
-            <!-- ITEM NUMBER 9 //-->
-            <li class="book even span-18 last">
-                <div class="span-18 last">
-                    <h2 class="title"><a href=used_book_listing.php><?php echo $book_title ?></a></h2>
-                    <span id="author"><?php echo $book_author_last . ', ' . $book_author_first; ?></span>
-                </div>
-                <div class="span-18 last">
-                    <div class="span-3">
-                        <a href="used_book_listing.php"><img class="span-3" src="<?php echo $book_cover_tiny ?>"/></a>
-                    </div>
-                    <div class="span-3">
-                        <p>Used: <span class="used_price">$<?php printf("%0.2f",$used_book_price); ?></span></p>
-                        <p>New: <span class="new_price">$<?php printf("%0.2f",$new_book_price); ?></span></p>
-                    </div>
-                    <div class="span-6">
-                        <p>Save $<span class="cc_savings"><?php printf("%0.2f",$ccsavings[9]);?> </span> on the price at your bookstore!</p>
-                    </div>
-                    <div class="span-6 last">
-                        <button class="add_to_cart" type="submit" value="book-<?php echo 9 ?>">+ Add to Cart</button>
-                    </div>
-                </div>
-            </li>
->>>>>>> .r53
         </ul>
     </div>
 <?php
