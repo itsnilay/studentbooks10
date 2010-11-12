1560013c1f3f8a3eb2015d7a237cce3e4c41f469
itsnilay <itsnilay@a2928fc4-1f20-58ce-a954-0d9b33963282>
2010-11-12 12:15:26 +0000

    BLOB functionality requirement

---
+++ SpindleTree/include/getBLOB.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('mysql_connect.php');
+
+$id = $_GET['id'];
+//$storedid = "10000";
+ // $id = (int)$storedid;
+
+if(!isset($id) || empty($id)){
+die("Please select your image!");
+}else{
+
+$query = SpindleTreeDB::getInstance()->get_book_image($id);
+$row = mysql_fetch_array($query);
+$content = $row['bookimage'];
+
+header('Content-type: image/jpg');
+echo $content;
+
+}
+
+?>
