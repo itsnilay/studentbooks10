b42fb3eab8eb11ea3470b52b08c739e5ed3cfa82
itsnilay <itsnilay@a2928fc4-1f20-58ce-a954-0d9b33963282>
2010-12-09 03:12:50 +0000

    Icon for URL...  Adding Images missing from repository.

--- SpindleTree/include/header.php
+++ SpindleTree/include/header.php
@@ -75,9 +75,10 @@ function draw_left_panel($sid){
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
+ <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
     <title><?php echo $page_title; ?></title>
+    <link rel="shortcut icon" type="image/ico" href="img/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="blueprint/screen.css" media="screen, projection" />
     <link rel="stylesheet" type="text/css" href="blueprint/print.css" media="print" />
     <link rel="stylesheet" type="text/css" href="blueprint/layout.css" media="all" />
